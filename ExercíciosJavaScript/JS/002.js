fbdc240f659b1c3de2804e52b47d356674cc03a6
Lucas Coelho <lucas.h.coelho20@gmail.com>
2024-04-12 02:41:58 -0300

    Exercícios feitos
    1 ao 4

---
+++ ExercíciosJavaScript/JS/002.js
@@ -0,0 +1,5 @@
+const olaNovamente = document.querySelector('h1');
+olaNovamente.textContent = "Olá mundo!";
+
+const olaSegundo = document.querySelector('p');
+olaSegundo.textContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit.Corrupti molestias facilis illo.Odit impedit illo dolore ex quia, ab magnam voluptate aperiam! Hic laudantium praesentium itaque nulla.Veniam, maxime molestiae."
